13bf92a20329352f25f9106daa9da73f0ab50d71
Mauro Bianchi <bianchimro@gmail.com>
2016-09-05 12:04:42 +0200

    data models

--- cromioshop/cromio/admin.py
+++ cromioshop/cromio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Product, ProductPrice
+from .models import Product, ProductPrice, Support
 
+admin.site.register(Support)
 admin.site.register(Product)
 admin.site.register(ProductPrice)
--- cromioshop/cromio/models.py
+++ cromioshop/cromio/models.py
@@ -7,10 +7,19 @@ from plata.product.models import ProductBase
 from plata.shop.models import PriceBase
 
 
+class Support(models.Model):
+    name = models.CharField(_('name'), max_length=100)
+    slug = models.SlugField(_('slug'), unique=True)
+    description = models.TextField(_('description'), blank=True)
+    #price in EUR
+    price = models.FloatField()
+
 class Product(ProductBase):
     name = models.CharField(_('name'), max_length=100)
     slug = models.SlugField(_('slug'), unique=True)
     description = models.TextField(_('description'), blank=True)
+    configurations = models.TextField(_('configurations'), blank=True)
+    accessories =  models.TextField(_('accessories'), blank=True)
 
     class Meta:
         ordering = ['name']
@@ -27,6 +36,9 @@ class Product(ProductBase):
 class ProductPrice(PriceBase):
     product = models.ForeignKey(Product, verbose_name=_('product'),
         related_name='prices')
+
+    material = models.CharField(max_length=300, null=True, blank=True)
+    size = models.CharField(max_length=300, null=True, blank=True)
 
     class Meta:
         get_latest_by = 'id'
